690258c61a1e903f76f0de12c39b92ebd9e4d77e
Nikul Goyani <goyaniniks@gmail.com>
2026-06-01 22:00:03 +0200

    feat: wire Experience section into page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../../components/Navbar';
 import Hero from '../../components/sections/Hero';
 import About from '../../components/sections/About';
+import Experience from '../../components/sections/Experience';
 import Skills from '../../components/sections/Skills';
 import Projects from '../../components/sections/Projects';
 import Contact from '../../components/sections/Contact';
@@ -9,9 +10,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="pt-16">
+      <main className="pt-20">
         <Hero />
         <About />
+        <Experience />
         <Skills />
         <Projects />
         <Contact />
