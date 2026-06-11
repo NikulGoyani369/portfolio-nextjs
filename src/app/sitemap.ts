9ff90a1b84de61ff22e75b189d4943bfb16ef5a0
Nikul Goyani <goyaniniks@gmail.com>
2026-06-11 19:46:26 +0200

    fix: remove unsupported sitemap fields for Next.js 13

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,8 +5,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://nikulgoyani.de',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
     },
   ];
 }
